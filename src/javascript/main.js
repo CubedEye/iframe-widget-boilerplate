24e8ea747cbbd833ff4333b953cbf0604c1857f7
Steven Robinson <cubed.eye@gmail.com>
2015-11-27 18:49:34 +1100

    Added some sample content.

---
+++ src/javascript/main.js
@@ -0,0 +1,6 @@
+(function(){
+    // Put your awesome javascript in here.
+    //
+    // Read the documentation on how to interact with Stile.
+    window.stileInterface = window.stileInterface || {};
+}())
